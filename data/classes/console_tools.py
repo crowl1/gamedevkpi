528a7c6e5ee9946b1b84c8fd20f8bd0268f56e3b
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-22 01:44:59 +0200

    edit main.py (build_wall) edit console_tools.py (send_wall)

--- data/classes/console_tools.py
+++ data/classes/console_tools.py
@@ -19,3 +19,11 @@ def game_mode_selection():
     '''
     print("1)PvP\n2)PvB\n3)BvB")
     pass
+
+def send_wall(wall):
+    if wall.coordinates_start.x == wall.coordinates_end.x:
+        print(
+            f"wall {chr(int(wall.coordinates_middle.y / 2 + 96 + 19)).capitalize()}{int((wall.coordinates_middle.x + 1) / 2)}h")
+    else:
+        print(
+            f"wall {chr(int(wall.coordinates_middle.y / 2 + 96 + 19)).capitalize()}{int((wall.coordinates_middle.x + 1) / 2)}v")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 
 
-from data.classes.console_tools import game_mode_selection
+from data.classes.console_tools import Tools, game_mode_selection, send_wall
+from data.classes.coordinate import Coordinate
 from data.classes.game import GameField, Player
 from data.classes.user import User
+from data.classes.wall import Wall
 
 
 def start_game():
@@ -32,8 +34,42 @@ def start_game():
     number_moves = 0  # Кількість ходів
 
 
-def build_wall():
-    pass
+def build_wall(player, game_field, list_of_players, counter=0):
+    Tools.clear_console() #перша лаба
+    # print_field(game_field.field) #перша лаба
+    if counter < 5:
+        if player.walls_amount > 0:
+            # place_the_wall_message() #перша лаба
+            wall_input = User.enter(player, "wall")
+            if wall_input == "back":
+                game(player, game_field, list_of_players)
+            else:
+                coordinates_split = wall_input.split(" ")
+                if len(coordinates_split) == 4:
+                    try:
+                        coordinates = [int(coordinate)
+                                       for coordinate in coordinates_split]
+                        wall = Wall(Coordinate(coordinates[0], coordinates[1]), Coordinate(
+                            coordinates[2], coordinates[3]), game_field)
+                        first = wall.if_there_path_to_win(
+                            game_field, list_of_players[0], list_of_players[1], wall)
+                        second = wall.between_two_pares
+                        third = wall.is_there_another_wall
+                        if first and second and not third:
+                            game_field.set_wall(wall)
+                            player.decrease_wall_amount()
+                            if player.player_type is False:
+                                send_wall(wall)
+                        else:
+                            build_wall(player, game_field,
+                                     list_of_players, counter + 1)
+                    except Exception as e:
+                        build_wall(player, game_field,
+                                 list_of_players, counter + 1)
+                else:
+                    build_wall(player, game_field, list_of_players, counter + 1)
+        else:
+            game(player, game_field, list_of_players)
 
 
 def move_player():
