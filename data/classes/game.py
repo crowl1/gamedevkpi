e1cd4876042bff5335b50410717eacebf1f41095
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-21 23:10:46 +0200

    edit game.py

--- data/classes/game.py
+++ data/classes/game.py
@@ -1,4 +1,5 @@
 from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
 
 
 class GameField:
@@ -12,3 +13,35 @@ class GameField:
     def set_graph(self):
         grid = Grid(matrix=self.graph_prepare(self.field))
         return grid
+    
+    def path_finder(self, players):
+        grid = self.graph
+        is_first_player_way = False
+        is_second_player_way = False
+
+        for win in players[0].for_win:
+            grid.cleanup()
+            start = grid.node(
+                players[0].current_position.y, players[0].current_position.x)
+            end = grid.node(win[1], win[0])
+
+            finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+            path, runs = finder.find_path(start, end, grid)
+            if len(path) >= 2:
+                is_first_player_way = True
+                break
+        for win in players[1].for_win:
+            grid.cleanup()
+            start = grid.node(
+                players[1].current_position.y, players[1].current_position.x)
+            end = grid.node(win[1], win[0])
+
+            finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+            path, runs = finder.find_path(start, end, grid)
+            if len(path) >= 2:
+                is_second_player_way = True
+                break
+        if is_first_player_way and is_second_player_way:
+            return True
+        else:
+            return False
