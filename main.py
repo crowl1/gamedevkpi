451e47c44fa254b08737b07554a04463b1f97a7c
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-22 01:38:18 +0200

    edit main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from data.classes.user import User
 
 
 def start_game():
-    game_field = GameField() # створюємо поле
+    game_field = GameField()  # створюємо поле
     first_player = Player(True, 1)
     second_player = Player(False, 2)
-    game_mode_selection() #перша лаба, вибір для гравця
+    game_mode_selection()  # перша лаба, вибір для гравця
     game_mode = User.ask_game_mode()
     if game_mode == "1":
         first_player = Player(True, 2)
@@ -26,12 +26,23 @@ def start_game():
         second_player = Player(False, 2)
     else:
         start_game()
-    
 
     list_of_players = [first_player, second_player]
     counter = 0
     number_moves = 0  # Кількість ходів
 
 
-if __name__=='__main__':
+def build_wall():
+    pass
+
+
+def move_player():
+    pass
+
+
+def game():
+    pass
+
+
+if __name__ == '__main__':
     start_game()
