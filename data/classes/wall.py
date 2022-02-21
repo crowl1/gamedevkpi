4380f6459f98e21d460ad663fe56dfc62667f3f1
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-21 22:54:11 +0200

    edit wall.py

--- data/classes/wall.py
+++ data/classes/wall.py
@@ -6,6 +6,7 @@ class Wall:
         self.coordinates_start = coordinates_start
         self.coordinates_end = coordinates_end
         self.coordinates_middle = self.set_coordinates_middle()
+        self.is_length_correct = self.if_length_correct()
 
     def set_coordinates_middle(self):
         if self.coordinates_start.y == self.coordinates_end.y:
@@ -18,3 +19,21 @@ class Wall:
                 return Coordinate(self.coordinates_start.x, self.coordinates_end.y + 1)
             else:
                 return Coordinate(self.coordinates_start.x, self.coordinates_start.y + 1)
+
+    def if_length_correct(self):
+        if self.coordinates_start.x == self.coordinates_end.x:
+            if self.coordinates_start.y > self.coordinates_end.y:
+                return True if len(
+                    [num for num in range(self.coordinates_end.y + 1, self.coordinates_start.y)]) == 1 else False
+            else:
+                return True if len(
+                    [num for num in range(self.coordinates_start.y + 1, self.coordinates_end.y)]) == 1 else False
+        elif self.coordinates_start.y == self.coordinates_end.y:
+            if self.coordinates_start.x > self.coordinates_end.x:
+                return True if len(
+                    [num for num in range(self.coordinates_end.x + 1, self.coordinates_start.x)]) == 1 else False
+            else:
+                return True if len(
+                    [num for num in range(self.coordinates_start.x + 1, self.coordinates_end.x)]) == 1 else False
+        else:
+            return False
