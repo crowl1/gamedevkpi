da53dc9d6bd68049cea40e93a2c0e40cc9bc3c2b
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-22 03:15:11 +0200

    edit wall.py (+if_between_two_pares)

--- data/classes/wall.py
+++ data/classes/wall.py
@@ -7,6 +7,7 @@ class Wall:
         self.coordinates_end = coordinates_end
         self.coordinates_middle = self.set_coordinates_middle()
         self.is_length_correct = self.if_length_correct()
+        self.between_two_pares = self._if_between_two_pares(game_field)
 
     def set_coordinates_middle(self):
         if self.coordinates_start.y == self.coordinates_end.y:
@@ -37,3 +38,16 @@ class Wall:
                     [num for num in range(self.coordinates_start.x + 1, self.coordinates_end.x)]) == 1 else False
         else:
             return False
+
+    def _if_between_two_pares(self, game_field):
+        if self.coordinates_start.y == self.coordinates_end.y:
+            return True if game_field.field[self.coordinates_start.x][self.coordinates_start.y - 1] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_start.x][self.coordinates_start.y + 1] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_start.x][self.coordinates_end.y - 1] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_start.x][self.coordinates_end.y + 1] in [0, 1,
+                                                                                                      2] else False
+        elif self.coordinates_start.x == self.coordinates_end.x:
+            return True if game_field.field[self.coordinates_start.x - 1][self.coordinates_start.y] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_start.x + 1][self.coordinates_start.y] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_end.x - 1][self.coordinates_start.y] in [0, 1, 2] and \
+                           game_field.field[self.coordinates_end.x + 1][self.coordinates_start.y] in [0, 1,
