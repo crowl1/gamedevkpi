d3a0e0e38a69965db2a5b33007874f2c7b2621a8
vslaOqhY <i.andriy.dovgalyuk@gmail.com>
2022-02-22 03:19:02 +0200

    edit wall.py (+if_there_another_wall)

--- data/classes/wall.py
+++ data/classes/wall.py
@@ -1,3 +1,5 @@
+import copy
+
 from data.classes.coordinate import Coordinate
 
 
@@ -8,6 +10,9 @@ class Wall:
         self.coordinates_middle = self.set_coordinates_middle()
         self.is_length_correct = self.if_length_correct()
         self.between_two_pares = self._if_between_two_pares(game_field)
+        self.is_there_another_wall = self._if_there_another_wall(
+            game_field.field)
+        self.is_there_path_to_win = None
 
     def set_coordinates_middle(self):
         if self.coordinates_start.y == self.coordinates_end.y:
@@ -50,4 +55,22 @@ class Wall:
             return True if game_field.field[self.coordinates_start.x - 1][self.coordinates_start.y] in [0, 1, 2] and \
                            game_field.field[self.coordinates_start.x + 1][self.coordinates_start.y] in [0, 1, 2] and \
                            game_field.field[self.coordinates_end.x - 1][self.coordinates_start.y] in [0, 1, 2] and \
-                           game_field.field[self.coordinates_end.x + 1][self.coordinates_start.y] in [0, 1,
+                           game_field.field[self.coordinates_end.x + 1][self.coordinates_start.y] in [0, 1, 2] else False
+
+    def _if_there_another_wall(self, game_field):
+        return False if game_field[self.coordinates_start.x][self.coordinates_start.y] == 3 and \
+                        game_field[self.coordinates_end.x][self.coordinates_end.y] == 3 and \
+                        game_field[self.coordinates_middle.x][self.coordinates_middle.y] == 5 else True
+
+    @staticmethod
+    def if_there_path_to_win(game_field, player1, player2, wall):
+        game_field.graph.cleanup()
+        temp_field = copy.deepcopy(game_field)
+        temp_field.set_wall(wall)
+        temp_field.graph = temp_field.set_graph()
+        if temp_field.path_finder([player1, player2]):
+            del temp_field
+            return True
+        else:
+            del temp_field
+            return False
